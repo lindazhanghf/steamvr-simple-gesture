821ba3dd5f02f421253b8bb214788cee16792b80
lindazhanghf <lindazhanghf@gmail.com>
2020-01-25 22:18:07 -0500

    Add raycast from camera script and random objects on table w/ materials

---
+++ Assets/Scripts/GraphicTest/CameraScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public GameObject[] objects;
+    public LayerMask layerMask;
+    public int num;
+
+    GameObject[] OutOfSight;
+
+    void Update()
+    {
+        num = 0;
+        foreach (GameObject obj in objects)
+        {
+            Debug.DrawRay(transform.position, obj.transform.position - transform.position);
+            if (Physics.Raycast(transform.position, obj.transform.position - transform.position, 20, layerMask, QueryTriggerInteraction.Collide))
+            {
+                num++;
+            }
+        }
+        Debug.Log(num);
+    }
+}
