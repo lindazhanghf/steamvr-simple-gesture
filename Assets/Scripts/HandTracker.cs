ca172ee50536e4363390401d2fa797bbac18fff9
zzhang391 <zzhang391@gatech.edu>
2020-09-08 02:46:37 -0400

    HandTracker: Implemented circle motion detection as described in the paper TraceMatch by Clarke et al. <https://dl.acm.org/doi/10.1145/2971648.2971714> NOTE: threshold not yet finalized

--- Assets/Scripts/HandTracker.cs
+++ Assets/Scripts/HandTracker.cs
@@ -7,9 +7,9 @@ public class HandTracker : MonoBehaviour
 {
     public enum HandType : int
     {
-        Left = 0,
+        Left = -1,
         Right = 1,
-        Any = 100,
+        Any = 0,
     }
 
     [Header("SteamVR References")]
@@ -25,23 +25,122 @@ public class HandTracker : MonoBehaviour
         get { return transform.position.y > Camera.position.y; }
     }
 
-    void OnTriggerEnter(Collider other)
+    // TraceMatch
+    public float TraceMatch_Threshold = 0.01f;
+    public float MinCircleRadius = 0.05f;
+    private Coroutine TraceMatchCoroutine;
+    private Vector3[] frames = new Vector3[30];
+    private int currFrame = 0;
+    private float circleRadius;
+    private float RadiusUpperBound
     {
-        GestureTrigger gestureTrigger = other.GetComponent<GestureTrigger>();
-        if (gestureTrigger == null) return;
-
-        if (gestureTrigger.Orientation != GestureSystem.Orientation.Default)
-        {
-            SameSideOfBody = (int)Hand == (int) gestureTrigger.Orientation;
-        }
-
-        if (gestureTrigger.Part == GestureSystem.BodyPart.Shoulder) OnShoulder = true;
+        get { return circleRadius + TraceMatch_Threshold; }
+    }
+    private float RadiusLowerBound
+    {
+        get { return circleRadius - TraceMatch_Threshold; }
     }
 
-    void OnTriggerExit(Collider other)
+    void OnEnable()
     {
-        GestureTrigger gestureTrigger = other.GetComponent<GestureTrigger>();
-        if (gestureTrigger == null) return;
+        TraceMatchCoroutine = StartCoroutine(TraceMatch());
+    }
+    
+    void OnDisable()
+    {
+        StopCoroutine(TraceMatchCoroutine);
+    }
 
-        if (gestureTrigger.Part == GestureSystem.BodyPart.Shoulder) OnShoulder = false;
-    }}
+    private IEnumerator TraceMatch()
+    {
+        int numFramesWithinThreshold = 0;
+        Vector3 originalPos = transform.position;
+        while (transform.position == originalPos)
+        {
+            yield return new WaitForSeconds(1f);
+        }
+        Debug.Log("TraceMatch : start receiving data");
+
+        currFrame = 0;
+        while (currFrame < frames.Length)
+        {
+            frames[currFrame] = transform.position;
+            currFrame++;
+            yield return new WaitForSeconds(0.03f); // 30 fps
+        }
+        Debug.Log("TraceMatch : collected first set of frames");
+
+        currFrame = 0;
+        while (true)
+        {
+            frames[currFrame] = transform.position;
+
+            // TraceMatch
+            Vector3 center = CalculateCenterOfCircle(frames[currFrame], frames[(currFrame + 10)%30], frames[(currFrame + 20)%30]);
+
+            if (center != Vector3.zero)
+            {
+                numFramesWithinThreshold = 0;
+                foreach (Vector3 frame in frames)
+                {
+                    if (WithinRadiusThreshold(Vector3.Distance(frame, center))) numFramesWithinThreshold++;
+                }
+
+                if (numFramesWithinThreshold > 25)
+                    Debug.Log(Hand.ToString() + " within = " + numFramesWithinThreshold);
+                else
+                    Debug.Log(Hand.ToString() + " < 25");
+            }
+
+            // Increment currFrame
+            currFrame++;
+            if (currFrame == frames.Length) currFrame = 0;
+
+            yield return new WaitForSeconds(0.03f); // 30 fps
+        }
+    }
+
+    /// <summary>
+    /// Finding the circumscribed circle using 3 points in 3D space
+    /// Reference: https://stackoverflow.com/a/13992781 
+    /// </summary>
+    private Vector3 CalculateCenterOfCircle(Vector3 p1, Vector3 p2, Vector3 p3)
+    {
+        // triangle "edges"
+        Vector3 t = p2-p1;
+        Vector3 u = p3-p1;
+        Vector3 v = p3-p2;
+
+        // triangle normal
+        Vector3 w = Vector3.Cross(t, u); 
+        float wsl = w.sqrMagnitude;
+
+        // area of the triangle is too small
+        if (Mathf.Approximately(wsl, 0f)) return Vector3.zero;
+
+        // helpers
+        float iwsl2 = 1.0f / (2.0f * wsl);
+        float tt = Vector3.Dot(t, t);
+        float uu = Vector3.Dot(u, u);
+        float vv = Vector3.Dot(v, v);
+        float uv = Vector3.Dot(u, v);
+        float tv = Vector3.Dot(t, v);
+
+        // NOTE: radius of the circle is saved in the class variable for other calculation
+        circleRadius = Mathf.Sqrt(tt * uu * (vv) * iwsl2 * 0.5f);
+        if (circleRadius < MinCircleRadius)
+        {
+            Debug.Log("TraceMatch :: circleRadius too small");
+            return Vector3.zero;
+        }
+
+        // result circle
+        Vector3 circCenter = p1 + (u*tt*(uv) - t*uu*(tv)) * iwsl2;
+        return circCenter;
+    }
+
+    private bool WithinRadiusThreshold(float distance)
+    {
+        return distance < RadiusUpperBound && distance > RadiusLowerBound;
+    }
+}
